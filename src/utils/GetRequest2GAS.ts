9e8088f36de3861ee1538d5b6daf23ae463dfac6
OHMURA Tokiya <tocky.dison@gmail.com>
2021-06-03 09:02:08 +0900

    Revert "install linter"
    This reverts commit 73bb3f8038c7877a2716513b2af1423b29343604.

--- src/utils/GetRequest2GAS.ts
+++ src/utils/GetRequest2GAS.ts
@@ -1,6 +1,6 @@
-import axios from "axios";
+import axios from "axios"
 const GasUrl = process.env.GAS_URL ?? "";
-console.log(GasUrl);
+
 export async function getRequest2GAS(params: { [key: string]: any }) {
   const res = await axios.get(
     GasUrl,

45c0b0fdb1b322c170db67cd765455a7315c61c5
OHMURA Tokiya <tocky.dison@gmail.com>
2021-06-07 18:21:17 +0900

    feat: add all params to register

--- src/pages/Register.tsx
+++ src/pages/Register.tsx
@@ -6,8 +6,16 @@ import { getRequest2GAS } from "../utils/GetRequest2GAS";
 import InfoMessage from "../components/InfoMessage";
 
 export default function Register() {
+  // 登録する情報
   const [name, setName] = useState("");
   const [carNumber, setCarNumber] = useState("");
+  const [carArea, setCarArea] = useState("");
+  const [carCategory, setCarCategory] = useState<number | undefined>();
+  const [carHiragna, setCarHiragna] = useState("");
+  const [carName, setCarName] = useState("");
+  const [companyName, setCompanyName] = useState("");
+  const [section, setSection] = useState("");
+
   const [isLoading, setIsLoading] = useState(false);
   const [carNunmberStatus, setCarNumberStatus] =
     useState<FormItemProps["validateStatus"]>("");
@@ -20,6 +28,12 @@ export default function Register() {
       mode: "register",
       name,
       car_number: carNumber,
+      car_area: carArea,
+      car_category: carCategory,
+      car_hiragana: carHiragna,
+      car_name: carName,
+      company_name: companyName,
+      section: section,
       crossDomain: true,
     };
     const res = await getRequest2GAS(params);
@@ -55,7 +69,15 @@ export default function Register() {
         <Form.Item label="名前" required>
           <Input onChange={(e) => setName(e.target.value)} value={name} />
         </Form.Item>
-        {/* TODO: エラーメッセージ（tooltipと同じで良さそう） */}
+        <Form.Item label="会社名">
+          <Input
+            onChange={(e) => setCompanyName(e.target.value)}
+            value={companyName}
+          />
+        </Form.Item>
+        <Form.Item label="セクション">
+          <Input onChange={(e) => setSection(e.target.value)} value={section} />
+        </Form.Item>
         <Form.Item
           label="車ナンバー"
           tooltip="「・」は0で埋め、かならず4桁の数字で入力してください"
@@ -69,6 +91,25 @@ export default function Register() {
             value={carNumber}
           />
         </Form.Item>
+        <Form.Item label="車地名" help="「品川」「練馬」など">
+          <Input onChange={(e) => setCarArea(e.target.value)} value={carArea} />
+        </Form.Item>
+        <Form.Item label="車ひらがな（1文字）">
+          <Input
+            onChange={(e) => setCarHiragna(e.target.value)}
+            value={carHiragna}
+          />
+        </Form.Item>
+        <Form.Item label="車のカテゴリー番号" help='"300"，"584"などの番号'>
+          <Input
+            onChange={(e) => setCarCategory(Number(e.target.value))}
+            value={carCategory}
+          />
+        </Form.Item>
+        <Form.Item label="車種">
+          <Input onChange={(e) => setCarName(e.target.value)} value={carName} />
+        </Form.Item>
+
         <Form.Item>
           <Button
             type="primary"

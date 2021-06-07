b8f7802f4bdfeaf47664d2745186217d6629d3b3
OHMURA Tokiya <tocky.dison@gmail.com>
2021-06-07 19:26:27 +0900

    feat: modify home layout

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,17 +1,19 @@
 import React from "react";
 import { Link } from "react-router-dom";
-import { Button, Layout } from "antd";
+import { Button, Layout, Space } from "antd";
 
 export default function Home() {
   return (
     <Layout.Content>
-      <h1>home</h1>
-      <Button type="primary" shape="round" size="large">
-        <Link to="/register">登録</Link>
-      </Button>
-      <Button type="primary" shape="round" size="large">
-        <Link to="/search">検索</Link>
-      </Button>
+      <h1>ホーム</h1>
+      <Space direction="vertical" style={{ width: "100%" }} size="large">
+        <Button type="primary" size="large" block>
+          <Link to="/register">登録</Link>
+        </Button>
+        <Button type="primary" size="large" block>
+          <Link to="/search">検索</Link>
+        </Button>
+      </Space>
     </Layout.Content>
   );
 }
